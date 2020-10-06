18d84a15f79779730b374c2a12c644f84fd3169e
ayyoub <afwallah@mobiblanc.com>
2020-10-06 18:00:58 +0100

    first commit

---
+++ script.sh
@@ -0,0 +1,27 @@
+DATABASE="test"
+USER="root"
+PASSWORD="root"
+FILE="/var/backups/mysql/${DATABASE}.$(date +"%m-%d-%Y-%T").sql.gz"
+if [ -f "$FILE" ]; then
+        echo " backup file  ${FILE} already exists !"
+else
+        mysqldump -u $USER -p$PASSWORD $DATABASE | gzip > $FILE  && find /var/backups/mysql/. -mtime +29 -exec rm {} \; || echo "Could not generate backup file ${FILE} file database ${DATABASE}"
+
+        if [ -f "$FILE" ]; then
+         echo "Generated new mysql backup file ${FILE} for database ${DATABASE}"
+           #scp $FILE username@server_ip:/path_to_remote_directory
+           #echo "password"
+        else
+         echo "Could not generate backup file ${FILE} file database ${DATABASE}"
+        fi
+
+fi
+#description 
+
+#execute mysqldump if everything goes right execute  #find /var/backups/mysql/. -mtime +29 -exec rm {} \; 
+#else echo Could not generate backup file 
+
+#find /var/backups/mysql/. -mtime +29 -exec rm {} \; simply deletes every file that is older than 29 days , we only need to keep the last 30 files 
+
+#crontab to execute script everyday at 1 am 
+#0 1 * * * /var/backups/script.sh
